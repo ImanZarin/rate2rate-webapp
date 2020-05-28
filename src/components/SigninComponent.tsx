99ca915f93cc1a09a0c6e06f30006f1c0f22294c
DESKTOP-ESNA9S1\iman zarin <zarin.iman@outlook.com>
2020-05-28 18:32:27 +0300

    formik

--- src/components/SigninComponent.tsx
+++ src/components/SigninComponent.tsx
@@ -1,13 +1,15 @@
 import { Component } from "react";
-import { Row, Label, Button } from "reactstrap";
+import { Row, Label, Button, Form } from "reactstrap";
 import React from "react";
-import { LocalForm, Control } from 'react-redux-form';
+import { Formik, Field } from 'formik';
 import { signinState } from '../shared/StateTypes';
+import * as yup from "yup";
 
 type myProps = {
     postSignin: (f: signinState) => void,
     username?: string
 }
+
 
 class SigninComponent extends Component<myProps> {
 
@@ -17,7 +19,14 @@ class SigninComponent extends Component<myProps> {
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
+    init: signinState = ({
+        email: "",
+        password: "",
+        username: ""
+    });
+
     handleSubmit(values: signinState) {
+        console.log("we get 1 with", values);
         this.props.postSignin(values);
     }
 
@@ -25,27 +34,53 @@ class SigninComponent extends Component<myProps> {
     render() {
         return (
             <div className="container">
-                <LocalForm onSubmit={this.handleSubmit}>
-                    <Row className="form-group">
-                        <Label htmlFor="username">User Name</Label>
-                        <Control.text model=".username" className="form-control"
-                            placeholder="choose a username" id="username" name="username" />
-                    </Row>
-                    <Row className="form-group" name="email">
-                        <Label htmlFor="email">Email</Label>
-                        <Control.text model=".email" className="form-control" placeholder="enter your email" />
-                    </Row>
-                    <Row className="form-group" name="password">
-                        <Label htmlFor="password">Password</Label>
-                        <Control.text model=".password" className="form-control" placeholder="choose a password" />
-                    </Row>
-                    <Button type="submit">Submit</Button>
-                </LocalForm>
+                <Formik initialValues={this.init} onSubmit={values => {
+                    console.log("we get 0 with", values);
+                    this.handleSubmit(values);
+                }} validationSchema={mySchema}>
+                    {({ values, handleSubmit, errors, touched }) => (
+                        <Form onSubmit={handleSubmit}>
+                            <Row className="form-group">
+                                <Label htmlFor="username">User Name</Label>
+                                <Field type="text" className="form-control"
+                                    placeholder="choose a username" id="username"
+                                    name="username" />
+                                <div style={{ visibility: errors.username && touched.username ? 'visible' : 'hidden', height: 40 }}
+                                    className="error-msg"> {errors.username}
+                                </div>
+                            </Row>
+                            <Row className="form-group" >
+                                <Label htmlFor="email">Email</Label>
+                                <Field type="text" className="form-control"
+                                    placeholder="enter your email" name="email" id="email" />
+                                <div style={{ visibility: errors.email && touched.email ? 'visible' : 'hidden', height: 40 }}
+                                    className="error-msg"> {errors.email}
+                                </div>
+                            </Row>
+                            <Row className="form-group" name="password">
+                                <Label htmlFor="password">Password</Label>
+                                <Field type="password" className="form-control" name="password"
+                                    placeholder="choose a password" id="password" />
+                                <div style={{ visibility: errors.password && touched.password ? 'visible' : 'hidden', height: 40 }}
+                                    className="error-msg"> {errors.password}
+                                </div>
+                            </Row>
+                            <Button type="submit" className="btn">Submit</Button>
+                        </Form>
+                    )}
+                </Formik>
                 <h1 >{this.props.username}</h1>
             </div>
         );
     }
 
 }
+
+const mySchema = yup.object().shape({
+    email: yup.string().matches(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1, 3}\.[0-9]{1, 3}\.[0-9]{1, 3}\.[0-9]{1, 3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, "invalid email")
+        .required(),
+    username: yup.string().min(4, "min 4 char").max(50, "too long").required("required"),
+    password: yup.string().min(4, "min 4 char").max(50, "too long").required("required"),
+});
 
 export default SigninComponent;
